928c0261ec11e2d69a36cd1c4da2aa733ccf98ec
Filip Radović <filipradovic05@gmail.com>
2020-04-24 21:50:52 +0200

    Bug fix

--- app/src/main/java/com/example/pokusaj/MainActivity.java
+++ app/src/main/java/com/example/pokusaj/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.pokusaj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.loginstrana);
+        setContentView(R.layout.prvastrana);
+        Button dug_login = findViewById(R.id.prijavi_se);
+        dug_login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.loginstrana);
+            }
+        });
+        Button dug_nastavi = findViewById(R.id.dugmereg);
+        dug_nastavi.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.searchstrana);
+            }
+        });
     }
 }
