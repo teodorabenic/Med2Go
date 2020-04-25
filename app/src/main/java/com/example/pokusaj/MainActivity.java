8660e2c6adb825959de2fc9c98af0669898739ec
Filip Radović <filipradovic05@gmail.com>
2020-04-25 12:03:55 +0200

    Ubacena mapa

--- app/src/main/java/com/example/pokusaj/MainActivity.java
+++ app/src/main/java/com/example/pokusaj/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pokusaj;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
         TextView dug_nastavi = findViewById(R.id.textView4);
         dug_nastavi.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                setContentView(R.layout.searchstrana);
+                Intent intent = new Intent(MainActivity.this, SearchStrana.class);
+                startActivity(intent);
             }
         });
     }
