e41c164e2bb37478bbd763ea1afcf05a59c73446
girica00 <nikola@lesic.me>
2020-04-25 16:41:39 +0200

    Dugmad :)

--- app/src/main/java/com/example/pokusaj/Login.java
+++ app/src/main/java/com/example/pokusaj/Login.java
@@ -7,16 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Registracija extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.napravitinaog);
-        Button sign_up = findViewById(R.id.buttonReg);
-        sign_up.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.loginstrana);
+        Button login = findViewById(R.id.button3);
+        login.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(Registracija.this, SearchStrana.class);
+                Intent intent = new Intent(Login.this, SearchStrana.class);
                 startActivity(intent);
             }
         });
